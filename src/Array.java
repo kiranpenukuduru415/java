dfc5566c2fca4b400c04a951274309625748d0af
kiran <kiran@DESKTOP-1JR0CIN>
2022-05-10 12:06:59 +0530

    added sdfsfsdfds

--- src/Array.java
+++ src/Array.java
@@ -1,16 +1,15 @@
-class Array{
-	    public static void main(String[] args) {  
-	        
-	   int [] nums = new int [] {20,30, 50, 70,40};  
-	         
-	        for (int i = 0; i < nums.length; i++) {  
-	          System.out.print(nums[i] + " ,");  
-	        }  
-	        System.out.println();  
-	         
-	       
-	        for (int i = nums.length-1; i >= 0; i--) {  
-	         System.out.print(nums[i] + " ,");  
-	        }  
-	    }  
-	}  
+class Array {
+	public static void main(String[] args) {
+
+		int[] nums = new int[] { 20, 30, 50, 70, 40 };
+
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ,");
+		}
+		System.out.println();
+
+		for (int i = nums.length - 1; i >= 0; i--) {
+			System.out.print(nums[i] + " ,");
+		}
+	}
+}
